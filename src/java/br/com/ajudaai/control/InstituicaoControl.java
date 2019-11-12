9ec6e4b00252bd137dfbb5fbb9218e90d7495a24
Sarah <>
2019-11-12 11:24:43 -0200

    Atualização das telas de cadastro

--- src/java/br/com/ajudaai/control/InstituicaoControl.java
+++ src/java/br/com/ajudaai/control/InstituicaoControl.java
@@ -24,6 +24,7 @@ import org.hibernate.Session;
 
 @ManagedBean(name = "instituicaoC")
 @ViewScoped
+//@SessionScoped
 public class InstituicaoControl implements Serializable {
 
     private List<Instituicao> instituicoes;
